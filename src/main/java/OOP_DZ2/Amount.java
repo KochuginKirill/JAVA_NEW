9b1ca926983ba631d1e8c5e9f8ca06dd6ae1cdfc
Kirill_Kochugin <kraingor@yandex.ru>
2023-10-17 11:00:42 +0300

    DZ2_OOP_corrected

--- src/main/java/OOP_DZ2/Amount.java
+++ src/main/java/OOP_DZ2/Amount.java
@@ -4,13 +4,4 @@ public interface Amount {
     double put(double putIn);
     double take(double putIn);
     double get();
-    //    public void put(double putIn){
-    //        this.balance += putIn;
-    //    }
-    //    public void take(double putIn){
-    //        this.balance -= putIn;
-    //    }
-    //    public double getAmount() {
-    //        return balance;
-    //    }
 }
