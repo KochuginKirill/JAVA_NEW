5df54d69971d66d6ad3eba1a6e216301663687f5
Kirill_Kochugin <kraingor@yandex.ru>
2023-11-13 19:41:39 +0200

    Change previous seminars

--- src/main/java/OOP_test/test.java
+++ src/main/java/OOP_test/test.java
@@ -2,11 +2,26 @@ package OOP_test;
 
 public class test {
     public static void main(String[] args) {
-        Worker w1 = new Worker ();
-        w1.firstName = "Имя_1";
-        w1.lastName = "Фамилия_1";
-        w1.salary = 100;
-        w1.id = 1000;
+        int decimalNumber = Integer.MAX_VALUE;
+        System.out.println("Десятичное число " + decimalNumber + " равно двоичному числу " + toBinary(decimalNumber));
+    }
+    public static String toBinary(int decimalNumber) {
+        double result = 0;
+        for(int i = 0; decimalNumber >= 0; i++){
+            result = result + ((decimalNumber % 2) * Math.pow(10, i));
+            decimalNumber = decimalNumber / 2;
+        }
+        return String.valueOf(result);
+    }
+
+    public static int toDecimal(String binaryNumber) {
+        int bin = Integer.parseInt(binaryNumber);;
+        double result = 0;
+        for(int i = 0; bin >= 0; i++){
+            result = result + ((bin % 10) * Math.pow(2, i));
+            bin = bin / 10;
+        }
+        return (int) result;
     }
 
 }
