821d86a1209496d78487e90c694fd6a81afcf432
Kirill_Kochugin <kraingor@yandex.ru>
2023-11-21 14:38:09 +0300

    OOP_DZ6 corrected

--- src/main/java/OOP_DZ6/ReportService.java
+++ src/main/java/OOP_DZ6/ReportService.java
@@ -9,7 +9,7 @@ public class ReportService {
         suportedTypeList.add(new PdfCreate());
         suportedTypeList.add(new XmlCreate());
         suportedTypeList.add(new JsonCreate());
-
+        // добавлять сюда
         SuportedReportsTypes CorrectType = null;
         for (int i = 0; i < suportedTypeList.size(); i++) {
             if(suportedTypeList.get(i).getFileType() == reportType.toUpperCase()){
